5b2d54e282439c976965ea8e2b86176aceaf6fb3
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 14:56:48 +0300

    add get request for create hero form

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -12,5 +12,10 @@ public class App {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model,"index.hbs");
         }, new HandlebarsTemplateEngine());
+
+        get("/hero-form",(request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
