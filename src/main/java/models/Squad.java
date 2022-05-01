f9ec6702f0c7621488ae35d55169b3a2fcb112d9
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 21:46:04 +0300

    add test method to check whether method creates new squad

--- src/main/java/models/Squad.java
+++ src/main/java/models/Squad.java
@@ -1,8 +1,13 @@
 package models;
 
 public class Squad {
+
     public Squad(String name, int size, String cause ){
 
     }
 
+    public static Squad createNewSquad() {
+        return new Squad("Teen Titans",5,"Crime fighting");
+
+    }
 }
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -7,7 +7,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class SquadTest {
     @Test
     public void newSquad_instantiatesCorrectly_true() {
-        Squad squad = new Squad("Avengers",5,"Infinity Stone");
+        Squad squad = new Squad("Teen Titans",5,"Crime fighting");
         assertTrue(squad instanceof Squad);
+    }
+
+    @Test
+    public void newSquad_methodInstantiatesCorrectly_true() {
+        Squad newSquad = Squad.createNewSquad();
+        assertTrue(newSquad instanceof Squad);
     }
 }
