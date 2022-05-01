1c040e97db49e41084fa6111cf62bf84e11e2b75
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 14:16:40 +0300

    add text and button to index.hbs to redirect users to a form to cerate a hero

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,4 +1,6 @@
 import java.util.HashMap;
+import java.util.Map;
+
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -6,6 +8,9 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
-
+        get("/", (request, response) -> {
+            Map<String, Object> model = new HashMap<>();
+            return new ModelAndView(model,"index.hbs");
+        }, new HandlebarsTemplateEngine());
     }
 }
