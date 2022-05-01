c7f7f387c45f61f776ffa10cf03758d1bf85947e
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 16:24:03 +0300

    add request to get all instances

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,6 +1,8 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import models.Hero;
 import spark.ModelAndView;
 import spark.template.handlebars.HandlebarsTemplateEngine;
 import static spark.Spark.*;
@@ -8,6 +10,7 @@ import static spark.Spark.*;
 public class App {
     public static void main(String[] args) { //type “psvm + tab” to autocreate this :)
         staticFileLocation("/public");
+
         get("/", (request, response) -> {
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model,"index.hbs");
@@ -16,6 +19,13 @@ public class App {
         get("/hero-form",(request, response) ->{
             Map<String, Object> model = new HashMap<>();
             return new ModelAndView(model, "hero-form.hbs");
+        }, new HandlebarsTemplateEngine());
+
+        get("/hero",(request, response) ->{
+            Map<String, Object> model = new HashMap<>();
+            ArrayList<Hero> hero = Hero.getAllInstances();
+            model.put("hero",hero);
+            return new ModelAndView(model, "hero.hbs");
         }, new HandlebarsTemplateEngine());
     }
 }
