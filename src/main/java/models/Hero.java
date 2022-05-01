ddd6f7a6e1530116989801deec52a7d298367706
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 13:20:34 +0300

    add test method to get name using the getter

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,11 +1,17 @@
 package models;
 
 public class Hero {
+    private String name;
     public Hero(String name, Integer age, String power, String weakness) {
+        this.name = name;
 
     }
 
     public static Hero createNewHero() {
         return new Hero("Catwoman",27,"Retractable claws","laser pointers");
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -5,16 +5,22 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
-    //create instance of hero
+    //create new instance
     @Test
     public void newHero_instantiatesCorrectly_true() {
         Hero testHero = new Hero("Catwoman",27,"Retractable claws","laser pointers");
         assertTrue(testHero instanceof Hero);
     }
-
+//use method to create new instance
     @Test
     public void newHero_methodInstantiatesCorrectly_true() {
         Hero newHero = Hero.createNewHero();
         assertTrue(newHero instanceof Hero);
+    }
+
+    @Test
+    public void newHero_getName_String() {
+        Hero newHero = Hero.createNewHero();
+        assertEquals("Catwoman",newHero.getName());
     }
 }
