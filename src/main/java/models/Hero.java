7ca6b0ceee7434b5168c16c79433ebc1c4250417
Kemunto Gift <giftoyugi@gmail.com>
2022-05-01 12:40:47 +0300

    create new test for new hero instances

--- src/main/java/models/Hero.java
+++ src/main/java/models/Hero.java
@@ -1,4 +1,7 @@
 package models;
 
 public class Hero {
+    public Hero(String name, Integer age, String power, String weakness) {
+
+    }
 }
--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,7 +1,16 @@
 package models;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class HeroTest {
+    //create instance of hero
+    @Test
+    public void newHero_instantiatesCorrectly_true() {
+        Hero testHero = new Hero("Catwoman",27,"Retractable claws","laser pointers");
+        assertTrue(testHero instanceof Hero);
+    }
+
 
 }
