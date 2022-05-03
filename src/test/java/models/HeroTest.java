d200070b7a5d533bb0f808e5df4e0cabcfeb2341
Kemunto Gift <giftoyugi@gmail.com>
2022-05-03 13:32:50 +0300

    commented out all tests

--- src/test/java/models/HeroTest.java
+++ src/test/java/models/HeroTest.java
@@ -1,78 +1,78 @@
-package models;
-
-import org.junit.After;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class HeroTest {
-    //create new instance
-    @Test
-    public void newHero_instantiatesCorrectly_true() {
-        Hero testHero = new Hero("Catwoman",27,"Retractable claws","laser pointers");
-        assertTrue(testHero instanceof Hero);
-    }
-//use method to create new instance
-    @Test
-    public void newHero_methodInstantiatesCorrectly_true() {
-        Hero newHero = Hero.createNewHero();
-        assertTrue(newHero instanceof Hero);
-    }
-
-    @Test
-    public void newHero_getName_String() {
-        Hero newHero = Hero.createNewHero();
-        assertEquals("Catwoman",newHero.getName());
-    }
-
-    @Test
-    public void newHero_getAge_Integer() {
-        Hero newHero = Hero.createNewHero();
-        assertEquals(27,newHero.getAge());
-    }
-
-    @Test
-    public void newHero_getPowers_String() {
-        Hero newHero = Hero.createNewHero();
-        assertEquals("Retractable claws",newHero.getPower());
-    }
-
-    @Test
-    public void newHero_getWeakness_String() {
-        Hero newHero = Hero.createNewHero();
-        assertEquals("laser pointers",newHero.getWeakness());
-    }
-    @After
-    public void tearDown() {
-        Hero.clearAllHeroes(); //clear out all the heroes before each test.
-    }
-
-    @Test
-    public void newHero_getAllInstances_true() {
-        Hero newHero = Hero.createNewHero();
-        Hero otherHero = Hero.createNewHero();
-        assertTrue(Hero.getAllInstances().contains(newHero));
-        assertTrue(Hero.getAllInstances().contains(otherHero));
-    }
-
-    @Test
-    public void getId_heroesInstantiateWithAnId_1() throws Exception{
-        Hero.clearAllHeroes();
-        Hero newHero = Hero.createNewHero();
-        Hero otherHero = Hero.createNewHero();
-        assertEquals(2,otherHero.getId());
-    }
-
-    @Test
-    public void findReturnsCorrectHero() throws Exception {
-        Hero newHero = Hero.createNewHero();
-        assertEquals(1, Hero.findById(newHero.getId()).getId());
-    }
-
-    @Test
-    public void findReturnsCorrectHeroWhenMoreThanOneHeroExists() throws Exception {
-        Hero newHero = Hero.createNewHero();
-        Hero otherHero = Hero.createNewHero();
-        assertEquals(2, Hero.findById(otherHero.getId()).getId());
-    }
-}
+// package models;
+//
+//import org.junit.After;
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class HeroTest {
+//    //create new instance
+//    @Test
+//    public void newHero_instantiatesCorrectly_true() {
+//        Hero testHero = new Hero("Catwoman",27,"Retractable claws","laser pointers");
+//        assertTrue(testHero instanceof Hero);
+//    }
+////use method to create new instance
+//    @Test
+//    public void newHero_methodInstantiatesCorrectly_true() {
+//        Hero newHero = Hero.createNewHero();
+//        assertTrue(newHero instanceof Hero);
+//    }
+//
+//    @Test
+//    public void newHero_getName_String() {
+//        Hero newHero = Hero.createNewHero();
+//        assertEquals("Catwoman",newHero.getName());
+//    }
+//
+//    @Test
+//    public void newHero_getAge_Integer() {
+//        Hero newHero = Hero.createNewHero();
+//        assertEquals(27,newHero.getAge());
+//    }
+//
+//    @Test
+//    public void newHero_getPowers_String() {
+//        Hero newHero = Hero.createNewHero();
+//        assertEquals("Retractable claws",newHero.getPower());
+//    }
+//
+//    @Test
+//    public void newHero_getWeakness_String() {
+//        Hero newHero = Hero.createNewHero();
+//        assertEquals("laser pointers",newHero.getWeakness());
+//    }
+//    @After
+//    public void tearDown() {
+//        Hero.clearAllHeroes(); //clear out all the heroes before each test.
+//    }
+//
+//    @Test
+//    public void newHero_getAllInstances_true() {
+//        Hero newHero = Hero.createNewHero();
+//        Hero otherHero = Hero.createNewHero();
+//        assertTrue(Hero.getAllInstances().contains(newHero));
+//        assertTrue(Hero.getAllInstances().contains(otherHero));
+//    }
+//
+//    @Test
+//    public void getId_heroesInstantiateWithAnId_1() {
+//        Hero.clearAllHeroes();
+//        Hero newHero = Hero.createNewHero();
+//        Hero otherHero = Hero.createNewHero();
+//        assertEquals(2,otherHero.getId());
+//    }
+//
+//    @Test
+//    public void findReturnsCorrectHero() {
+//        Hero newHero = Hero.createNewHero();
+//        assertEquals(1, Hero.findById(newHero.getId()).getId());
+//    }
+//
+//    @Test
+//    public void findReturnsCorrectHeroWhenMoreThanOneHeroExists() {
+//        Hero newHero = Hero.createNewHero();
+//        Hero otherHero = Hero.createNewHero();
+//        assertEquals(2, Hero.findById(otherHero.getId()).getId());
+//    }
+//}
--- src/test/java/models/SquadTest.java
+++ src/test/java/models/SquadTest.java
@@ -1,89 +1,89 @@
-package models;
-
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.*;
-
-class SquadTest {
-    @Test
-    public void newSquad_instantiatesCorrectly_true() {
-        Squad squad = new Squad("Teen Titans", 5, "Crime fighting");
-        assertTrue(squad instanceof Squad);
-    }
-
-    @Test
-    public void newSquad_methodInstantiatesCorrectly_true() {
-        Squad newSquad = Squad.createNewSquad();
-        assertTrue(newSquad instanceof Squad);
-    }
-
-    @Test
-    public void newSquad_getName_String() {
-        Squad newSquad = Squad.createNewSquad();
-        assertEquals("Teen Titans", newSquad.getSquadName());
-    }
-
-    @Test
-    public void newSquad_getSize_Int() {
-        Squad newSquad = Squad.createNewSquad();
-        assertEquals(5, newSquad.getSquadSize());
-    }
-
-    @Test
-    public void newSquad_getCause_String() {
-        Squad newSquad = Squad.createNewSquad();
-        assertEquals("Crime fighting", newSquad.getCause());
-    }
-
-    @Test
-    public void newSquad_getAllInstances_true() {
-        Squad newSquad = Squad.createNewSquad();
-        Squad otherSquad = Squad.createNewSquad();
-        assertTrue(Squad.getInstances().contains(newSquad));
-        assertTrue(Squad.getInstances().contains(otherSquad));
-    }
-
-   // @AfterAll
-    //public void tearDown() {
-      //  Squad.clearAllSquadMembers(); //clear out all the squad members before each test.
-   //
-
-    @Test
-    public void newSquad_getSquadMembers_Array() {
-        Squad newSquad = Squad.createNewSquad();
-        Hero newHero = Hero.createNewHero();
-        newSquad.createSquadMembers(newHero);
-        assertEquals("Catwoman", newSquad.getSquadMembers().get(0).getName());
-    }
-
-    @Test
-    public void newSquad_allTestSquadMembers_Array() {
-        Hero newHero = Hero.createNewHero();
-        Squad newSquad = Squad.createNewSquad();
-        Squad.clearAllSquadMembers();
-        newSquad.getSquadMembers().add(newHero);
-        newSquad.getSquadMembers().add(newHero);
-        assertEquals("Catwoman", newSquad.getSquadMembers().get(0).getName());
-    }
-
-    @Test
-    public void addMember_addsMemberToSquad_Hero() {
-        Hero newHero = Hero.createNewHero();
-        Squad testSquad = Squad.createNewSquad();
-        Squad newSquad = Squad.findBySquadId(1);
-        Squad.clearAllSquadMembers();
-        newSquad.getSquadMembers().add(newHero);
-        newSquad.getSquadMembers().add(newHero);
-        assertEquals(2, newSquad.getSquadMembers().size());
-    }
-
-    @Test
-    public void setNewMember_hero() {
-        Hero.clearAllHeroes();
-        Hero newHero = Hero.createNewHero();
-        Squad testSquad = Squad.createNewSquad();
-        testSquad.createSquadMembers(newHero);
-
-        assertEquals(1, testSquad.getSquadMembers().get(0).getId());
-    }
-}
+//package models;
+//
+//import org.junit.jupiter.api.Test;
+//
+//import static org.junit.jupiter.api.Assertions.*;
+//
+//class SquadTest {
+//    @Test
+//    public void newSquad_instantiatesCorrectly_true() {
+//        Squad squad = new Squad("Teen Titans", 5, "Crime fighting");
+//        assertTrue(squad instanceof Squad);
+//    }
+//
+//    @Test
+//    public void newSquad_methodInstantiatesCorrectly_true() {
+//        Squad newSquad = Squad.createNewSquad();
+//        assertTrue(newSquad instanceof Squad);
+//    }
+//
+//    @Test
+//    public void newSquad_getName_String() {
+//        Squad newSquad = Squad.createNewSquad();
+//        assertEquals("Teen Titans", newSquad.getSquadName());
+//    }
+//
+//    @Test
+//    public void newSquad_getSize_Int() {
+//        Squad newSquad = Squad.createNewSquad();
+//        assertEquals(5, newSquad.getSquadSize());
+//    }
+//
+//    @Test
+//    public void newSquad_getCause_String() {
+//        Squad newSquad = Squad.createNewSquad();
+//        assertEquals("Crime fighting", newSquad.getCause());
+//    }
+//
+//    @Test
+//    public void newSquad_getAllInstances_true() {
+//        Squad newSquad = Squad.createNewSquad();
+//        Squad otherSquad = Squad.createNewSquad();
+//        assertTrue(Squad.getInstances().contains(newSquad));
+//        assertTrue(Squad.getInstances().contains(otherSquad));
+//    }
+//
+//   // @AfterAll
+//    //public void tearDown() {
+//      //  Squad.clearAllSquadMembers(); //clear out all the squad members before each test.
+//   //
+//
+//    @Test
+//    public void newSquad_getSquadMembers_Array() {
+//        Squad newSquad = Squad.createNewSquad();
+//        Hero newHero = Hero.createNewHero();
+//        newSquad.createSquadMembers(newHero);
+//        assertEquals("Catwoman", newSquad.getSquadMembers().get(0).getName());
+//    }
+//
+//    @Test
+//    public void newSquad_allTestSquadMembers_Array() {
+//        Hero newHero = Hero.createNewHero();
+//        Squad newSquad = Squad.createNewSquad();
+//        Squad.clearAllSquadMembers();
+//        newSquad.getSquadMembers().add(newHero);
+//        newSquad.getSquadMembers().add(newHero);
+//        assertEquals("Catwoman", newSquad.getSquadMembers().get(0).getName());
+//    }
+//
+//    @Test
+//    public void addMember_addsMemberToSquad_Hero() {
+//        Hero newHero = Hero.createNewHero();
+//        Squad testSquad = Squad.createNewSquad();
+//        Squad newSquad = Squad.findBySquadId(1);
+//        Squad.clearAllSquadMembers();
+//        newSquad.getSquadMembers().add(newHero);
+//        newSquad.getSquadMembers().add(newHero);
+//        assertEquals(2, newSquad.getSquadMembers().size());
+//    }
+//
+//    @Test
+//    public void setNewMember_hero() {
+//        Hero.clearAllHeroes();
+//        Hero newHero = Hero.createNewHero();
+//        Squad testSquad = Squad.createNewSquad();
+//        testSquad.createSquadMembers(newHero);
+//
+//        assertEquals(1, testSquad.getSquadMembers().get(0).getId());
+//    }
+//}
